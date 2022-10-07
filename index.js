84979f10f07ff7c8f4cbf234fd5d1ad5644bb545
Daniel Sanchez <danxschz@gmail.com>
2022-10-07 01:27:15 -0400

    Build contact methods section

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// Navigation 
 const scrollIntoElement = (e) => {
   e.preventDefault();
   const element = document.querySelector(`.${e.target.getAttribute('id')}`);
-  element.scrollIntoView({ behavior: "smooth" });
+  element.scrollIntoView({ behavior: 'smooth' });
 }
 
 const landingBtn = document.querySelector('#landing');
@@ -20,4 +21,11 @@ const contactBtn = document.querySelector('#contact');
 contactBtn.addEventListener('click', scrollIntoElement);
 contactBtn.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' || e.key === ' ') scrollIntoElement(e);
+});
+
+// Check my work button
+const checkBtn = document.querySelector('#check');
+checkBtn.addEventListener('click', () => {
+  const projects = document.querySelector('.projects');
+  projects.scrollIntoView({ behavior: 'smooth' });
 });
