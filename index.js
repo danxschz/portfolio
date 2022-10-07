57ecdfd2ec18aac9df5721c497481f7610fbddeb
Daniel Sanchez <danxschz@gmail.com>
2022-10-07 02:49:53 -0400

    Improve scrolling function to avoid covering top of elements  with fixed nav and implement animations on scroll

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Navigation 
+const getElementPosition = (element) => {
+  const rect = element.getBoundingClientRect();
+
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  }
+}
+
 const scrollIntoElement = (e) => {
   e.preventDefault();
   const element = document.querySelector(`.${e.target.getAttribute('id')}`);
-  element.scrollIntoView({ behavior: 'smooth' });
+  const position = getElementPosition(element).top;
+
+  // I'm using this method instead of element.scrollIntoView() so that I can offset the sticky nav (height of 66px)
+  window.scroll({
+    top: position - 66,
+    behavior: 'smooth'
+  });
 }
 
 const landingBtn = document.querySelector('#landing');
@@ -27,5 +42,10 @@ contactBtn.addEventListener('keypress', (e) => {
 const checkBtn = document.querySelector('#check');
 checkBtn.addEventListener('click', () => {
   const projects = document.querySelector('.projects');
-  projects.scrollIntoView({ behavior: 'smooth' });
+  const position = getElementPosition(projects).top;
+
+  window.scroll({
+    top: position - 66,
+    behavior: 'smooth'
+  });
 });
